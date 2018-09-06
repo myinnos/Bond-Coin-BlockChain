84a916ac78d452edbcd7fa6ae86ac3805677290f
MyInnos <prabhakar_thota@live.com>
2018-09-06 17:09:10 +0530

    block

--- app/src/main/java/in/myinnos/bondcoin/BondCoinActivity.java
+++ app/src/main/java/in/myinnos/bondcoin/BondCoinActivity.java
@@ -3,7 +3,7 @@ package in.myinnos.bondcoin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class BondCoinActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
